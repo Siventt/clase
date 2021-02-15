48ca54b6798a49c7025c70cd46cc87d312a01ea4
Siventt <72410600+Siventt@users.noreply.github.com>
2021-02-15 14:49:06 +0100

    Delete cliente directory

--- cliente/anterior/radioCircu.js
+++
@@ -1,4 +0,0 @@
-var r = parseInt(prompt("Radio"));
-
-document.write(`El area de una circunferencia con radio ${r} es ${Math.PI*r*r}`);
-
--- cliente/u9/modulos.js
+++
@@ -1,6 +0,0 @@
-export{
-    areaCirculo,
-}
-function areaCirculo(radio){
-    return Math.PI * radio**2;
-}
